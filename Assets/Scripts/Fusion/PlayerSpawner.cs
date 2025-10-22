ade538ae16c06d6a6b68f00dbf4d83eaaeb927ad
Matt <148502464+darkdastrevas@users.noreply.github.com>
2025-10-22 13:51:33 -0300

    ajustes_random

--- Assets/Scripts/Fusion/PlayerSpawner.cs
+++ Assets/Scripts/Fusion/PlayerSpawner.cs
@@ -1,26 +1,25 @@
 ﻿using Fusion;
 using UnityEngine;
-
 public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
 {
     [Header("Prefabs dos Jogadores")]
-    public GameObject Player1Prefab;
+    public GameObject Player1Prefab; 
     public GameObject Player2Prefab;
-
+    // Contador de jogadores que se juntaram (compartilhado com todo mundo '-')
+    private static int playerCount = 0;
     public void PlayerJoined(PlayerRef player)
     {
-        // No Shared Mode, todos recebem o evento,
-        // então apenas o próprio player deve spawnar o seu objeto
         if (player != Runner.LocalPlayer)
             return;
+ 
+        Vector3 spawnPos = new Vector3(playerCount * 2, 1, 0);
+        // Escolhe o prefab com base no número de jogadores
+        GameObject prefabToSpawn = playerCount == 0 ? Player1Prefab : Player2Prefab;
+        // Spawna o objeto 
+        
 
-        // Define a posição inicial baseada no ID do jogador
-        Vector3 spawnPos = new Vector3(player.PlayerId * 2, 1, 0);
-
-        // Escolhe o prefab com base no ID do jogador
-        GameObject prefabToSpawn = player.PlayerId == 0 ? Player1Prefab : Player2Prefab;
-
-        // Spawna o objeto (não precisa de autoridade extra)
         Runner.Spawn(prefabToSpawn, spawnPos, Quaternion.identity, player);
+        // Incrementador
+        playerCount++;
     }
 }
